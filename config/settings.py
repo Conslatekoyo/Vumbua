5ef7ecc2b17912f7ab97f319e4085d6fa129f545
Conslatekoyo <conslatekoyo@gmail.com>
2025-03-25 19:23:32 +0300

    Added additional requirements

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)uq#krm!fb3ywxb57zif9f&8ki67bnhesfsho7psj-9=*u^63g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', True,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vumbua-production.up.railway.app','127.0.0.1']
+CSRF_TRUSTED_ORIGINS=['https://vumbua-production.up.railway.app']
 
 
 # Application definition
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True  # Change this in production
 
@@ -79,13 +81,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+else:
+ import dj_database_url
+
+ DATABASES = {
+    'default': {dj_database_url.config(conn_max_age=500)}
+}
+
 
 
 # Password validation
@@ -122,7 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT= BASE_DIR/'staticfiles'
+STATICFILES_DIRS= [BASE_DIR/'static']
 STATIC_URL = 'static/'
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
